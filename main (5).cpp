fd456abff48d054b7cb70807df9d5571495da8b0
neil1040 <108753558+neil1040@users.noreply.github.com>
2022-07-05 22:37:07 +0530

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a;
+ char b;
+ float c;
+ bool d;
+ long e;
+ double f;
+ cout<<"size of a "<<sizeof(a)<<endl;
+  cout<<"size of b "<<sizeof(b)<<endl;
+   cout<<"size of c "<<sizeof(c)<<endl;
+    cout<<"size of d "<<sizeof(d)<<endl;
+     cout<<"size of e "<<sizeof(e)<<endl;
+      cout<<"size of f "<<sizeof(f);
+}
