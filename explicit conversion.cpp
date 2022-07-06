67e037b1b8f6fe98defba39dbec38c1eb959b76e
neil kumar <108753558+neil1040@users.noreply.github.com>
2022-07-06 21:38:14 +0530

    Add files via upload

---
+++ explicit conversion.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a = 10;
+  float af=(float)a;
+  cout<<af;
+  return 0;
+}
---
+++ main (7).cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int rollno;
+char gender;
+float cgpa;
+cin>>rollno;
+cin>>gender;
+cin>>cgpa;
+cout<<"rollno is "<<rollno<<endl;
+cout<<"gender is "<<gender<<endl;
+cout<<"cgpa is "<<cgpa;
+
+
+  return 0;
+}
