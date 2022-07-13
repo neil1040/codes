1a1544826accb7ee83e957686bf35c40be91fae8
neil kumar <108753558+neil1040@users.noreply.github.com>
2022-07-13 21:40:07 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+long factorial(int a)
+{
+	if(a>1)
+	return(a*factorial(a-1));
+	else 
+	return 1;
+}
+int main()
+{
+	long number;
+	cin>>number;
+	cout<<number<<"!="<<factorial(number);
+	return 0;
+}
+
